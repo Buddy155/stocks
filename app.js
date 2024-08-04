a2a103d8dbf5f39d19ea1b0b3c5466f35460f2cc
Buddy155 <alistairmaul@gmail.com>
2024-08-04 18:17:58 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,17 +7,29 @@ document.addEventListener("DOMContentLoaded", function() {
     var svg1 = document.getElementById('svg1id');
     //graph points
     var VAR = 100;
-    var autoval = 0;
+    var autoval = 50;
     
-    
-    
+    var varx = 500;
+    var vary = 0;
+    var tot = "";
+    var points = "00,00 20,20 40,40 60,60 80,80 100,100 120,120 140,140 160,160 180,180 200,200 220,220 240,240 260,260 280,280 300,300 320,320 340,340 360,360 380,380 400,400 420,420 440,440 460,460 480,480 500,50";
+    var polyline = document.getElementById('line');
+
     var intervalID = setInterval(function () {
 
         // Your logic here
-        autoval = autoval +1;
+        autoval = autoval +20;
+        points = "00,00 20,20 40,40 60,60 80,80 100,100 120,120 140,140 160,160 180,180 200,200 220,220 240,240 260,260 280,280 300,300 320,320 340,340 360,360 380,380 400,400 420,420 440,440 460,460 480,480 500,50"+tot;
+        if (vary < -1 && vary > 500) {
+            vary = vary+getRandomIntNeg(-30, 50);
+        }
+        else {
+            vary = vary+getRandomIntPos(-30, 0)
+        }
         varx = varx+20;
-        vary = vary+20
-        var tot = tot+" "+varx+","+vary
+        
+        
+        tot = tot+" "+varx+","+vary;
         console.log("autoval"+autoval);
         if (++x === 50000) {
             window.clearInterval(intervalID);
@@ -25,12 +37,10 @@ document.addEventListener("DOMContentLoaded", function() {
         var sliderviewboxoutput =  autoval+" 0 500 500";
         console.log(sliderviewboxoutput);
         svg1.setAttribute('viewBox', sliderviewboxoutput);
-        var points = "00,00 20,20 40,40 60,60 80,80 100,100 120,120 140,140 160,160 180,180 200,200 220,220 240,240 260,260 280,280 300,300 320,320 340,340 360,360 380,380 400,400 420,420 440,440 460,460 480,480 500,50"+tot;
-        var varx = 500;
-        var vary = 0;
-        var polyline = document.getElementById('line');
+        
+        console.log("tot"+tot);
         polyline.setAttribute('points', points);
-        console.log(points);
+        console.log("points"+points);
         
      }, 1000);
     const slider = document.getElementById('myRange');
@@ -42,4 +52,14 @@ document.addEventListener("DOMContentLoaded", function() {
         console.log(sliderviewboxoutput);
         svg1.setAttribute('viewBox', sliderviewboxoutput);
     };
+    function getRandomIntNeg(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+    }
+    function getRandomIntPos(min, max) {
+        const minCeiled = Math.ceil(min);
+        const maxFloored = Math.floor(max);
+        return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+    }
 });
