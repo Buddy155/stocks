8cbefee0ab7008418b9b2a2641c1e88cd3ce098f
Buddy155 <alistairmaul@gmail.com>
2024-08-05 17:18:57 -0400

    Update app.js

--- app.js
+++ app.js
@@ -23,10 +23,11 @@ document.addEventListener("DOMContentLoaded", function() {
     
 
     var intervalID = setInterval(function () {
-
-        // Your logic here
+        
+       
+        
         autoval = autoval +10;
-        points = "00,00 20,20 40,40 60,60 80,80 100,100 120,120 140,140 160,160 180,180 200,200 220,220 240,240 260,260 280,280 300,300 320,320 340,340 360,360 380,380 400,400 420,420 440,440 460,460 480,480 500,50"+tot;
+        
         if (vary > 49 && vary < 450) {
             vary = vary+getRandomIntNeg(-30, 30);
             //console.log("NORMAL");
@@ -40,10 +41,18 @@ document.addEventListener("DOMContentLoaded", function() {
             //console.log("FORCE UP");
         }
         varx = varx+10;
+        if (x==0) {
+            console.log("FIRST TOT"+tot);
+        }
+        
+        tot = " "+varx+","+vary;
         //console.log(vary);
+        if (x==0) {
+            console.log("SECOND"+tot);
+        }
+        console.log(x+" NUMEROOOO"+tot);
         
-        
-        tot = tot+" "+varx+","+vary;
+        points = points+tot;
         //console.log("autoval"+autoval);
         if (++x === 50000) {
             window.clearInterval(intervalID);
@@ -54,11 +63,20 @@ document.addEventListener("DOMContentLoaded", function() {
         
         //onsole.log("tot"+tot);
         polyline.setAttribute('points', points);
-        //console.log("points"+points);
+        console.log("points"+points);
         if (++x === 10) {
             daychange();
         }
+        var five = points.split(" ")[0];
+        console.log("five"+five);
+        deletenum = getlength(five)+1;
+        console.log("pointb"+points);
+        points=points.slice(deletenum);
+        console.log("DeleteNum"+deletenum)
+        console.log("after"+points)
+        console.log("aftertot"+tot)
         
+    
      }, 100);
     const slider = document.getElementById('myRange');
     slider.oninput = function() {
@@ -82,7 +100,7 @@ document.addEventListener("DOMContentLoaded", function() {
     function daychange()
     {
         //show ui about day ending
-        points = "00,00 20,20 40,40 60,60 80,80 100,100 120,120 140,140 160,160 180,180 200,200 220,220 240,240 260,260 280,280 300,300 320,320 340,340 360,360 380,380 400,400 420,420 440,440 460,460 480,480 500,50"+tot;
+        points = points+tot;
         day = day+1;
         varx = 500;
         var vary = 0;
@@ -98,5 +116,7 @@ document.addEventListener("DOMContentLoaded", function() {
             box.style.opacity = opacityValue;
         }, 1000);
     }
-    
+    function getlength(number) {
+        return number.toString().length;
+    }
 });
