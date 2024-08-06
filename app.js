ab084daf1a5fb029228ac5f35d39c5fb6af2525e
Buddy155 <alistairmaul@gmail.com>
2024-08-05 21:57:11 -0400

    update

--- app.js
+++ app.js
@@ -16,16 +16,27 @@ document.addEventListener("DOMContentLoaded", function() {
     var points = "00,00 20,20 40,40 60,60 80,80 100,100 120,120 140,140 160,160 180,180 200,200 220,220 240,240 260,260 280,280 300,300 320,320 340,340 360,360 380,380 400,400 420,420 440,440 460,460 480,480 500,50";
     var polyline = document.getElementById('line');
     var daypop = 0;
-
+    var whensold = 0;
+    var whenbought = 0;
+    var multiplier = 0;
+    var profit = 0;
     let opacityValue = 1;
+    var curval = 0;
+    var money = 500;
     const box = document.getElementById('frame2');
     
-   
-    
+    document.getElementById("buy").onclick = function() {buy()};
+    document.getElementById("sell").onclick = function() {sell()};
 
     var intervalID = setInterval(function () {
         
-       
+        multiplier = curval/whenbought;
+        multiplier = Math.round(multiplier * 100) / 100
+        if (whenbought == 0)
+        {
+            multiplier = 0;
+        }
+        
         
         autoval = autoval +10;
         
@@ -42,10 +53,16 @@ document.addEventListener("DOMContentLoaded", function() {
             //console.log("FORCE UP");
         }
         varx = varx+10;
-        var curval = 500-vary
+        curval = 500-vary
         let valdis1 = document.getElementById("valdis");
-        valdis1.innerHTML = curval;
-        console.log(curval);
+        valdis1.innerHTML = "Value:"+curval;
+        let profitdis = document.getElementById("profit");
+        profitdis.innerHTML = "Profit:"+profit;
+        let multiplierdis = document.getElementById("multiplier");
+        multiplierdis.innerHTML = "Multiplier:"+multiplier;
+        let moneydis = document.getElementById("money");
+        moneydis.innerHTML = "Money:"+money+"$";
+        //console.log(curval);
         if (x==0) {
             //console.log("FIRST TOT"+tot);
         }
@@ -80,14 +97,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 //console.log("pointb"+points);
                 points=points.slice(deletenum);
                 //console.log("DeleteNum"+deletenum)
-                console.log("after"+points)
+                //console.log("after"+points)
                 //console.log("aftertot"+tot)
             }
             
         }
         
+        console.log(multiplier);
+        console.log(profit);
         
-    
      }, 100);
     const slider = document.getElementById('myRange');
     slider.oninput = function() {
@@ -130,8 +148,23 @@ document.addEventListener("DOMContentLoaded", function() {
                 daypop = 0;
             }, 3000);
         }, 1000);
+        
     }
     function getlength(number) {
         return number.toString().length;
+    }
+    function sell() {
+        
+        whensold = curval;
+        console.log("profit"+profit);
+        console.log("multiplier"+multiplier);
+        console.log("sold for "+whensold+"!");
+        multiplier = 0;
+        profit = profit+money*multiplier-money;
+        money = money + money*multiplier-money
+    }
+    function buy() {
+        whenbought = curval;
+        console.log("bought for "+whenbought+"!");
     }
 });
